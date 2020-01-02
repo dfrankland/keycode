130e4b92654926f7a38a2dab79aeec6b0a94dab7
Dylan Frankland <dfrankland@users.noreply.github.com>
2020-01-01 17:00:19 -0800

    Fix build script for keycode_macro

--- keycode_macro/build.rs
+++ keycode_macro/build.rs
@@ -1,6 +1,7 @@
-use std::{env, fs::File, io::Write, path::Path};
-
+#[cfg(test)]
 fn main() {
+    use std::{env, fs::File, io::Write, path::Path};
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("keycode_converter.rs");
 
@@ -12,3 +13,6 @@ fn main() {
     let mut file = File::create(&dest_path).unwrap();
     file.write_all(keycode_converter_data.as_bytes()).unwrap();
 }
+
+#[cfg(not(test))]
+fn main() {}
